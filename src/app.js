8c8eb1250aebd374bf76f0831a691749083778b5
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-19 21:56:26 +0100

    feat: imprement create a new repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -15,7 +14,16 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repositorie = {
+    "id": uuid(),
+    "title": title,
+    "url": url,
+    "techs": techs,
+    "likes": 0
+  }
+  repositories.push(repositorie);
+  response.send(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
